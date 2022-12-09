d9742bea832f1f02b080c3f62d55f27a30cad939
Kellthuzad <kellen.f.swain@gmail.com>
2022-12-09 06:00:58 +0000

    DANG

--- Day09/Solution.py
+++ Day09/Solution.py
@@ -1,12 +1,64 @@
 
 def main():
     lines = parse()
+    snake = []
+    tailHistory.add((0,0))
 
-    print(lines)
+    # if you want part 1, set this to 2
+    chunks = 10
+    for _ in range(chunks):
+        snake.append((0,0))
+
+    for line in lines:
+        direction, magnitude = line.split()
+        mag = int(magnitude)
+        moveHead(direction, mag, snake)
+    print(len(tailHistory))
+
+tailHistory = set()
+ 
+def moveHead(direction, magnitude, snake):
+    delta = (0,0)
+
+    if direction == 'U':
+        delta = (0,1)
+    elif direction == 'D':
+        delta = (0,-1)
+    elif direction == 'R':
+        delta = (1,0)
+    elif direction == 'L':
+        delta = (-1,0)
+
+    for i in range(magnitude):
+        snake[0] = (snake[0][0] + delta[0], snake[0][1] + delta[1])
+        moveTail(snake, 0, delta)
+
+
+def moveTail(snake, i, delta):
+    global tailHistory
+    if abs(snake[i][0] - snake[i+1][0]) > 1 or abs(snake[i][1] - snake[i+1][1]) > 1:
+        dx = 1
+        dy = 1
+        if snake[i][0] < snake[i+1][0]:
+            dx *= -1
+        if snake[i][1] < snake[i+1][1]:
+            dy *= -1
+
+        if snake[i][0] == snake[i+1][0]:
+            snake[i+1] = (snake[i+1][0], snake[i+1][1] + dy)
+        elif snake[i][1] == snake[i+1][1]:
+            snake[i+1] = (snake[i+1][0] + dx, snake[i+1][1])
+        else:
+            snake[i+1] = (snake[i+1][0] + dx, snake[i+1][1] + dy)
+        
+        if i+1 == len(snake) - 1:
+            tailHistory.add(snake[i+1])
+        else:
+            moveTail(snake, i+1, delta)
+
 
 def parse():
-    lines = [l.strip() for l in open("./Day6/input.txt")]
+    lines = [l.strip() for l in open("./Day09/input.txt")]
     return lines
 
 main()
-        
