4bb47aee3ad4c7fd319e1da3c660993717f2df76
Kellthuzad <kellen.f.swain@gmail.com>
2022-12-16 15:33:28 +0000

    screams of pain

--- Day15/Solution.py
+++ Day15/Solution.py
@@ -1,12 +1,79 @@
 
 def main():
-    lines = parse()
+    targetLine = 2000000
+    sensorBeaconPairs, interference = parse(targetLine)
+    # print(calculateSensorZone(sensorBeaconPairs, interference, targetLine))
+    print(slicinAndDicin(sensorBeaconPairs))
 
-    print(lines)
 
-def parse():
-    lines = [l.strip() for l in open("./Day6/input.txt")]
-    return lines
+
+def calculateSensorZone(sensorBeaconPairs, interference, targetLine):
+    targetSet = set()
+    for sensor, beacon in sensorBeaconPairs:
+        dx = abs(sensor[0] - beacon[0])
+        dy = abs(sensor[1] - beacon[1])
+        distance = dx + dy
+        populateTargetLine(sensor, distance, targetLine, targetSet, interference)
+    return(len(targetSet))
+
+def slicinAndDicin(pairs):
+
+    for y in range(4000001):
+        ranges = []
+        for (sX, sY), (bX, bY) in pairs:
+            distanceFromTargetLine = abs(y-sY)
+            distance = abs(sX-bX) + abs(sY-bY)
+
+            
+            remainder = distance - distanceFromTargetLine
+            if remainder < 0:
+                continue
+
+            ranges.append((sX - remainder, sX + remainder))
+        ranges.sort()
+
+        final = []
+        currentMin, currentMax = ranges[0]
+        for minX, maxX in ranges[1:]:
+            if minX-1 <= currentMax:
+                currentMax = max(currentMax, maxX)
+            else:
+                final.append((minX, maxX))
+                currentMin, currentMax = minX, maxX
+        final.append((currentMin, currentMax))
+
+        if len(final) > 1:
+            x = final[0][1]
+            return x * 4000000 + y
+
+def populateTargetLine(sensor, distance, targetLine, targetSet, interference):
+    distanceFromTargetLine = abs(targetLine - sensor[1])
+    if distanceFromTargetLine > distance:
+        return
+    
+    remainder = distance - distanceFromTargetLine
+    for i in range(sensor[0]-remainder, sensor[0]+remainder+1):
+        if i not in interference:
+            targetSet.add(i)
+
+
+def parse(targetLine):
+    lines = [l.strip() for l in open("./Day15/input.txt")]
+    sensorBeaconPairs = []
+    objectInterference = set()
+    for line in lines:
+        chunks = line.split()
+        sX = int(chunks[2].strip(',').strip('x='))
+        sY = int(chunks[3].strip(':').strip('y='))
+        bX = int(chunks[8].strip(',').strip('x='))
+        bY = int(chunks[9].strip(':').strip('y='))
+        sensorBeaconPairs.append( ( (sX,sY),(bX,bY) ) )
+
+        if sY == targetLine:
+            objectInterference.add(sX)
+        if bY == targetLine:
+            objectInterference.add(bX)
+    return sensorBeaconPairs, objectInterference
 
 main()
         
--- Day16/Solution.py
+++ Day16/Solution.py
@@ -1,12 +1,133 @@
+from copy import deepcopy
 
 def main():
-    lines = parse()
+    valves, nonZeroValves = parse()
 
-    print(lines)
+    #print(dfs(valves['AA'], valves, nonZeroValves, set(), 0, 0))
+    print(dfs2(valves['AA'], valves['AA'],valves, nonZeroValves, set(), 0, 0, 0)) 
+
+def dfs(currentValve, valves, nonZeroValves, openedValves, minute, totalReleasedPressure):
+    releasePressures = [totalReleasedPressure]
+    if minute > 30:
+        return 0
+    for valve in nonZeroValves:
+        if valve in openedValves:
+            continue
+        minutesToOpen = distanceFromValve(currentValve, valve, valves) +1
+        totalReleasedPressure += ((30 - (minute +minutesToOpen)) * valves[valve].flowRate)
+        minute += minutesToOpen
+        openedValves.add(valve)
+        releasePressures.append(dfs(valves[valve], valves, nonZeroValves, openedValves, minute, totalReleasedPressure))
+        openedValves.remove(valve)
+        minute -= minutesToOpen
+        totalReleasedPressure -= ((30 - (minute +minutesToOpen)) * valves[valve].flowRate)
+
+    
+    return max(releasePressures)
+
+
+def dfs2(currentValve1, currentValve2, valves, nonZeroValves, openedValves, minute1, minute2, totalReleasedPressure):
+    releasePressures = [totalReleasedPressure]
+    if minute1 > 26 or minute2 > 26:
+        return 0
+    for valve1 in nonZeroValves:
+        if valve1 in openedValves:
+            continue
+        for valve2 in nonZeroValves:
+            if valve2 in openedValves or valve1 == valve2:
+                continue
+            minutesToOpen1 = distanceFromValve(currentValve1, valve1, valves) +1
+            minutesToOpen2 = distanceFromValve(currentValve2, valve2, valves) +1
+
+            totalReleasedPressure += ((26 - (minute1 +minutesToOpen1)) * valves[valve1].flowRate)
+            totalReleasedPressure += ((26 - (minute2 +minutesToOpen2)) * valves[valve2].flowRate)
+            minute1 += minutesToOpen1
+            minute2 += minutesToOpen2
+            openedValves.add(valve1)
+            openedValves.add(valve2)
+            releasePressures.append(dfs2(valves[valve1], valves[valve2], valves, nonZeroValves, openedValves, minute1, minute2, totalReleasedPressure))
+            openedValves.remove(valve1)
+            openedValves.remove(valve2)
+            minute1 -= minutesToOpen1
+            minute2 -= minutesToOpen2
+            totalReleasedPressure -= ((26 - (minute1 +minutesToOpen1)) * valves[valve1].flowRate)
+            totalReleasedPressure -= ((26 - (minute2 +minutesToOpen2)) * valves[valve2].flowRate)
+
+    
+    return max(releasePressures)
+
+
+def greedy(valves):
+    currentValve = valves['AA']
+    openedValves = set()
+    minute = 0
+    totalReleasedPressure = 0
+
+    while minute <= 30:
+        highestValue = (0,'',0)
+        for valveName, valve in valves.items():
+            if valveName in openedValves or valve.flowRate == 0:   
+                continue
+            minutesTaken = distanceFromValve(currentValve, valveName, valves) +1
+            if minutesTaken + minutesTaken > 30:
+                continue
+            pressureValue = (valve.flowRate * (30 - (minute + minutesTaken)))/minutesTaken
+            #pressureValue = valve.flowRate/minutesTaken
+            if pressureValue > highestValue[0]:
+                highestValue = (pressureValue, valveName, minutesTaken)
+        
+        if highestValue[0] == 0:
+            return totalReleasedPressure
+        
+        _, valveName, minutesTaken = highestValue
+        totalReleasedPressure += ((30 - (minute +minutesTaken)) * valves[valveName].flowRate)
+        minute += minutesTaken
+        openedValves.add(valveName)
+        currentValve = valves[valveName]
+
+    return totalReleasedPressure 
+
+            
+
+
+def distanceFromValve(origin, destination, valves):
+    queue = [(origin, 1)]
+    visited = set()
+    visited.add(origin.name)
+    while queue:
+        currentValve, distance = queue.pop(0)
+        for valve in currentValve.valves:
+            if valve in visited:
+                continue
+            if valve != destination:
+                visited.add(valve)
+                queue.append((valves[valve], distance+1))
+            else:
+                return distance
+            
 
 def parse():
-    lines = [l.strip() for l in open("./Day6/input.txt")]
-    return lines
+    lines = [l.strip() for l in open("./Day16/input.txt")]
+    valves = {}
+    nonZeroValves = []
+    for line in lines:
+        chunks = line.split()
+        flowRate = int(chunks[4].strip(';').strip('rate='))
+        valves[chunks[1]] = Valve(chunks[1], flowRate, chunks[9:])
+        if flowRate > 0:
+            nonZeroValves.append(chunks[1])
+
+    return valves, nonZeroValves
+
+class Valve:
+    def __init__(self, name, flowRate, sources):
+        self.name = name
+        self.flowRate = flowRate
+        self.valves = []
+        for source in sources:
+            self.valves.append(source.strip(','))
+
+
 
 main()
         
